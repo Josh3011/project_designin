81344072882d3a818c4501e89c4420d4d6757441
Josh3011 <joshaadrista@gmail.com>
2022-05-29 20:19:21 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-<<<<<<< HEAD
 //Route Detail Jasa
 Route::get('listjasa1', function () {
-=======
-//Route List Jasa
-Route::get('listjasa', function () {
->>>>>>> e5b53a066a0a0673f194e42f9c5e01d9e9a86f6a
     return view('daftarjasa.listjasa1');
 });
 Route::get('listjasa2', function () {
